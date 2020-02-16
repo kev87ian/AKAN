ff77bb33a19f8a87b4fca167fa1e4187f2b18168
kev87ian <kelvinian87@gmail.com>
2020-02-16 22:41:48 +0300

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# AKAN
+# Akan name generator
+ By Kevin Mathenge.
+## Description
+This is an application that helps one to know the day they were born and generate for them an *Akan* Ghanaian name depending on their gender and the day they were born.
+
+## BDD
+This application functions in such a way that it allows you to key in your credentials. It accepts your gender, date of birth(day, month year) then it displays the exact day of birth and your Akan name. 
+## Techologies used
+This application was developed by incorporating the use of:
+- HTML 
+- CSS
+- Bootstrap
+- Javascript 
+## Contacts and support
+For more information, corrections or clarification, reach out on kelvinian87@gmail.com
+## License
+The MIT License (MIT)
+
+Copyright (c) 2019 Kevin Mathenge
---
+++ js/script.js
@@ -0,0 +1,58 @@
+function findName() {
+  var dd = parseInt(document.getElementById("dd").value);
+  var mm = parseInt(document.getElementById("mm").value);
+  var yy = parseInt(document.getElementById("yy").value);
+  var femaleGender = document.getElementById("femaleGender");
+  var maleGender = document.getElementById("maleGender");
+  var date = new Date(yy + "-" + mm + "-" + dd);
+  var dayBorn = date.getDay();
+
+  var maleNames = [
+    "Kwasi",
+    "Kwado",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+  ];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  if (dd < 1 || dd > 31) {
+    alert("Enter valid day");
+  // } else if (dd.toString().length !== 2) {
+    alert("Enter a valid day!");
+  }
+  if (mm < 1 || mm > 12) {
+    alert("Enter valid month");
+  }
+  // else if (mm.toString().length !== 2) {
+  //   alert("Enter a valid month");
+  // }
+  if (yy.toString().length !== 4) {
+    alert("Enter a valid year");
+  } else if (maleGender.checked) {
+    alert(
+      "Hello there, your day of birth is " +
+        daysOfTheWeek[dayBorn] +
+        " and your Akan name is " +
+        maleNames[dayBorn]
+    );
+  } else {
+    alert(
+      "Hello there, your day of birth is " +
+        daysOfTheWeek[dayBorn] +
+        " and your Akan name is " +
+        maleNames[dayBorn]
+    );
+  }
+}
