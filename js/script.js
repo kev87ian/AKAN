e1e0f599c9a41ab4bc4b2825fadb812faac605f5
kev87ian <kelvinian87@gmail.com>
2020-02-20 17:32:50 +0300

    fixed indenting

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function akanName() {
-  var dd = parseInt(document.getElementById("dd").value); 
+  var dd = parseInt(document.getElementById("dd").value);
   var mm = parseInt(document.getElementById("mm").value);
   var yy = parseInt(document.getElementById("yy").value);
   var femaleGender = document.getElementById("femaleGender");
   var maleGender = document.getElementById("maleGender");
-  var date = new Date(yy + "-" + mm + "-" + dd); 
+  var date = new Date(yy + "-" + mm + "-" + dd);
   var dayBorn = date.getDay();
 
-  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"]; // array of male names
+  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; // array of male names
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; // array of female names
   var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // array of days
 
@@ -23,16 +23,16 @@ function akanName() {
   } else if (maleGender.checked) {
     alert(
       "Hello Sir, your day of birth is " + // output
-        daysOfTheWeek[dayBorn] +
-        ", and your Akan name is " +
-        maleNames[dayBorn]
+      daysOfTheWeek[dayBorn] +
+      ", and your Akan name is " +
+      maleNames[dayBorn]
     );
   } else {
     alert(
       "Hello Lady, your day of birth is " + // output
-        daysOfTheWeek[dayBorn] +
-        ", and your Akan name is " +
-        femaleNames[dayBorn] 
+      daysOfTheWeek[dayBorn] +
+      ", and your Akan name is " +
+      femaleNames[dayBorn]
     );
   }
 }
